d4c046515586dd805dd8c02972df4161f832b790
Breno Baroni <breno_baroni@hotmail.com>
2019-09-09 21:49:43 -0300

    Aula Versionamento

---
+++ RestWithAsp.NetUdemy-4-Versionamento/RestWithAsp.NetUdemy/Controllers/PersonsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestWithAsp.NetUdemy.Model;
+using RestWithAsp.NetUdemy.Services;
+
+namespace RestWithAsp.NetUdemy.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private IPersonService _personService;
+
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personService.Create(person));
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personService.Update(person));
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
