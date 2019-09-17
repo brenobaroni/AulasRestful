04d477c570936869da248940b32b7030cb70b904
Breno Baroni <breno_baroni@hotmail.com>
2019-09-16 22:33:00 -0300

    Iniciado Negotiation

---
+++ RestWithAsp.NetUdemy-10-Negotiation/RestWithAsp.NetUdemy/Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using RestWithAsp.NetUdemy.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAsp.NetUdemy.Model
+{
+    [Table("persons")]
+    public class Person : BaseEntity
+    {
+        [Column("FirstName")]
+        public string FirstName { get; set; }
+
+        [Column("LastName")]
+        public string LastName { get; set; }
+
+        [Column("Address")]
+        public string Address { get; set; }
+
+        [Column("Gender")]
+        public string Gender { get; set; }
+    }
+}
